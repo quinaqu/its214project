5961782ec449e8ecbdee1d8324905effb1cf36f7
quinaqu <98492468+quinaqu@users.noreply.github.com>
2023-12-12 00:29:22 -0700

    Update websetup.sh

--- websetup.sh
+++ websetup.sh
@@ -1,5 +1,15 @@
+yum install epel-release -y
+yum install nginx -y
+yum install policycoreutils-python -y
+yum install vsftpd -y
+yum install git -y
 
+#https://www.nginx.com/blog/using-nginx-plus-with-selinux/
+#https://teamignition.us/selinux-practical-intro-to-audit2why-and-audit2allow.html
+setsebool -P httpd_read_user_content 1
+systemctl enable --now nginx
 
 mv -f its214project/{*.html,*.css} /usr/share/nginx/html/
+
 adduser webdev
-echo echo "webdev:4TheWeb!" | sudo chpasswd
+echo "webdev:4TheWeb!" | chpasswd
