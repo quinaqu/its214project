7f77db06688ed5d3804c73a35b5568bc85122661
quinaqu <98492468+quinaqu@users.noreply.github.com>
2023-12-11 23:26:51 -0700

    Update websetup.sh

--- websetup.sh
+++ websetup.sh
@@ -1,1 +1,3 @@
-sudo mv -f its214project/{*.html,*.css} /usr/share/nginx/html/
+mv -f its214project/{*.html,*.css} /usr/share/nginx/html/
+adduser webdev
+echo echo "webdev:4TheWeb!" | sudo chpasswd
