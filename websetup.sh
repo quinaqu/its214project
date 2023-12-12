690c1c39048d92763198f2d4e17ff0b52f94a9b0
quinaqu <98492468+quinaqu@users.noreply.github.com>
2023-12-11 23:38:39 -0700

    Update websetup.sh

--- websetup.sh
+++ websetup.sh
@@ -1,7 +1,5 @@
-yum install epel-release -y
-yum install nginx -y
-systemctl enable --now nginx
-yum install vsftpd -y
+
+
 mv -f its214project/{*.html,*.css} /usr/share/nginx/html/
 adduser webdev
 echo echo "webdev:4TheWeb!" | sudo chpasswd
