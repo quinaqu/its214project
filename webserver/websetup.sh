140174ccc7244b7037daeab58595a287ee56333b
quinaqu <98492468+quinaqu@users.noreply.github.com>
2023-12-12 05:30:32 -0700

    Update websetup.sh

--- webserver/websetup.sh
+++ webserver/websetup.sh
@@ -17,19 +17,22 @@ systemctl enable vsftpd
 sed -i 's#anonymous_enable=YES#anonymous_enable=NO#' /etc/vsftpd/vsftpd.conf
 sed -i 's/#chroot_list_enable=YES/chroot_list_enable=YES/' /etc/vsftpd/vsftpd.conf
 sed -i 's:#xferlog_file=/var/log/xferlog:xferlog_file=/var/log/xferlog:' /etc/vsftpd/vsftpd.conf
-sed -i 's:userlist_enable=YES:userlist_enable=NO:' /etc/vsftpd/vsftpd.conf
 sed -i '7,$d' /etc/vsftpd/user_list
 
 echo "userlist_deny=NO">> /etc/vsftpd/vsftpd.conf
+echo 'allow_writeable_chroot=YES' >> /etc/vsftpd/vsftpd.conf
 echo "webdev" >> /etc/vsftpd/user_list
 echo "webdev" > /etc/vsftpd/chroot_list
+setsebool -P ftpd_full_access 1
+systemctl restart vsftpd
 
 mv -f its214project/webserver/{*.html,*.css} /usr/share/nginx/html/
 
-#I tried a few methods for encrypting the passwords in the script but got lazy (and they're already exposed in the assignment)
+#i looked at options for encrypting the passwords in the script but got lazy (and they're already exposed in the assignment)
 groupadd webdevs
 adduser webdev
 sudo echo "4TheWeb!" | passwd "webdev" --stdin
 chgrp -R webdevs /usr/share/nginx
+chmod g+rwx /usr/share/nginx
 usermod -d /usr/share/nginx webdev
 usermod -aG webdevs webdev
