00e2a607d978158cd58647ef642dc2de3f621da2
quinaqu <98492468+quinaqu@users.noreply.github.com>
2023-12-12 00:48:43 -0700

    Update websetup.sh

--- websetup.sh
+++ websetup.sh
@@ -2,7 +2,7 @@ yum install epel-release -y
 yum install nginx -y
 yum install policycoreutils-python -y
 yum install vsftpd -y
-yum install git -y
+
 
 #https://www.nginx.com/blog/using-nginx-plus-with-selinux/
 #https://teamignition.us/selinux-practical-intro-to-audit2why-and-audit2allow.html
