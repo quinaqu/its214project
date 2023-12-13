54cee706f3cc795596ddc3b7e49a2bc979ab7430
quinaqu <98492468+quinaqu@users.noreply.github.com>
2023-12-12 23:48:41 -0700

    Update dbsetup.sh

--- database/dbsetup.sh
+++ database/dbsetup.sh
@@ -14,3 +14,8 @@ sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | mysql_secure_installation
     y # Remove test database and access to it?
     y # Reload privilege tables now?
 EOF
+
+mysql -uroot -ptest1234 -e "CREATE USER 'dbdev'@'localhost' IDENTIFIED BY 'projectfinal'; GRANT ALL PRIVILEGES ON *.* TO 'dbdev@localhost'; FLUSH PRIVILEGES;"
+
+firewall-cmd --zone=project --add-port=1433/tcp --permanent
+firewall-cmd –reload
