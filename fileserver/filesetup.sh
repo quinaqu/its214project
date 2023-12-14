08daf02a34583f02bb9a2ba869f79d3ab31c0f9e
quinaqu <98492468+quinaqu@users.noreply.github.com>
2023-12-13 23:23:43 -0700

    Update filesetup.sh

--- fileserver/filesetup.sh
+++ fileserver/filesetup.sh
@@ -3,15 +3,17 @@ systemctl enable smb.service
 systemctl enable nmb.service
 setsebool -P samba_enable_home_dirs 1
 systemctl restart smb.service
-mkdir /shares/fileshare
-chcon -R -t samba_share_t /fileshare
+mkdir /srv/fileshare
+chcon -R -t samba_share_t /srv/fileshare
 
 firewall-cmd --permanent --zone=project --add-service=samba
 systemctl restart firewalld
 
 cat >> /etc/samba/smb.conf << EOF
 [fileshare]
-path = /fileshare
-valid users = username
+path = /srv/fileshare
+valid users = admin client webdev vagrant
 read only = no
+public = no
+browsable = yes
 EOF
